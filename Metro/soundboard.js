61fdf0e16fed3306536d18bccf52223952812e2f
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-10-21 14:02:56 -0700

    added notes to soundboard

--- Metro/soundboard.js
+++ Metro/soundboard.js
@@ -2,9 +2,51 @@
 
 const soundBoard = [{
     name : 'C',
+    value: '../' //path to assets and respective sound
+},
+{
+    name: 'C-Sharp',
     value: '../'
 },
 {
-
+    name: 'D',
+    value: '../',
 },
-]
+{
+    name: 'D-Sharp',
+    value: '../',
+},
+{
+    name: 'E',
+    value: '../',
+},
+{
+    name: 'F',
+    value: '../',
+},
+{
+    name: 'F-Sharp',
+    value: '../',
+},
+{
+    name: 'G',
+    value: '../',
+},
+{
+    name: 'G-Sharp',
+    value: '../',
+},
+{
+    name: 'A',
+    value: '../',
+},
+{
+    name: 'A-Sharp',
+    value: '../',
+},
+{
+    name: 'B',
+    value: '../',
+}];
+
+export { soundBoard };
