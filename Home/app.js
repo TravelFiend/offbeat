f9affba99406df7e44289aa3e74648d8becabfc9
TravelFiend <mikegrace274@gmail.com>
2019-10-21 13:58:53 -0700

    additional scaffolding added

---
+++ Home/app.js
@@ -0,0 +1,13 @@
+export const storeUser = (user) => {
+    const jsonUser = JSON.stringify(user);
+    localStorage.set('user', jsonUser);
+};
+
+export const getUser = () => {
+    const jsonUser = localStorage.getItem('user');
+    if (!jsonUser){
+        return null;
+    }
+    const user = JSON.parse(jsonUser);
+    return user;
+};
