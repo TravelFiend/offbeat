28c3acb09d717b53b05b186d08a21d6dbf2a2d46
Ian Andrewson <ianandrewson@gmail.com>
2019-10-22 14:18:26 -0700

    linted

--- Utils/generateMetroSoundList.js
+++ Utils/generateMetroSoundList.js
@@ -2,9 +2,9 @@ import { generateMetroSoundListItem } from '../Utils/generateMetroSoundListItem.
 
 export const generateMetroSoundList = (allSounds) => {
     const menu = document.createElement('select');
-    menu.name = "metronome-sound";
-    menu.id = "metronome-sound-menu";
-    menu.textContent = "metronome-sound";
+    menu.name = 'metronome-sound';
+    menu.id = 'metronome-sound-menu';
+    menu.textContent = 'metronome-sound';
 
     allSounds.forEach(obj => {
         menu.append(generateMetroSoundListItem(obj));
