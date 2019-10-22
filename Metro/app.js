5aa9cbd8e178bc73d51ed8b29856ec9efcdd5c42
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-10-22 14:32:55 -0700

    same

--- Metro/app.js
+++ Metro/app.js
@@ -11,7 +11,7 @@ let soundBoard = new SoundBoard(soundBoards[0]);
 
 for (let i = 0; i < soundBoard.length; i++){
     if (!soundBoards[0][i].value){
-        soundBoards[0][i] === soundBoards[0][0];
+        soundBoards[0][i] = soundBoards[0][0];
     }
 }
 
