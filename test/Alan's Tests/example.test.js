2e1e7bf62863463583d35fe92e7a5eee63ab9442
Ian Andrewson <ianandrewson@gmail.com>
2019-10-22 15:45:35 -0700

    did some more stuff with Alan's tests. Travis Fix 002

--- test/Alan's Tests/example.test.js
+++ test/Alan's Tests/example.test.js
@@ -8,7 +8,7 @@ test('function will trigger sound', function(assert) {
     //Arrange
     // Set up your parameters and expectations
     event.target.value = '../../assets/middle-C-clarinet.wav';
-    const audioFileToPlay = '../../assets/middle-C-clarinet.wav';
+    //const audioFileToPlay = '../../assets/middle-C-clarinet.wav';
     //Act 
     // Call the function you're testing and set the result to a const
     triggerSound();
