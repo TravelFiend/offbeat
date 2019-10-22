b2b9197bf9b297b86cf530ecd2a44f20cd9952c6
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-10-22 13:10:12 -0700

    added if statement to complete the keyboard if the soundboard isnt complete

--- Metro/app.js
+++ Metro/app.js
@@ -8,6 +8,11 @@ import { soundBoards } from './data/soundboards.js';
 let note;
 
 let soundBoard = soundBoards[0];
+for (let i = 0; i < soundBoard.length; i++){
+    if (!soundBoards[0][i].value){
+        soundBoards[0][i] === soundBoards[0][0];
+    }
+}
 
 let BPMElement;
 
