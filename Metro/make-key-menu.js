fe4606464f8d3ec5cd66a342edcb0ac1e6608fa2
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-10-23 10:11:14 -0700

    add sounds

---
+++ Metro/make-key-menu.js
@@ -0,0 +1,7 @@
+import { masterSoundList } from masterSoundList.js;
+
+let blackKeys = document.getElementsByClassName('black-keys');
+let whiteKeys = document.getElementsByClassName('white-keys');
+let soundsSelectDropDown = document.createElement('select');
+
+
