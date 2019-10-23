7c49a02eb96807f08d49831c9217141fed986f28
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-10-22 17:29:03 -0700

    same

--- Metro/mapsound.js
+++ Metro/mapsound.js
@@ -1,10 +1,12 @@
 import { triggerSound } from '../Common/trigger-sound.js';
+import { populateLegend } from '../utils/populate-sound-bank.js';
 export const mapSound = (soundBoard, note) => {
     for (let i = 0; i < Object.keys(soundBoard).length; i++){
         let poop = soundBoard[i];
         let name = poop.name;
+        populateLegend(poop);
         note = document.getElementById(name);
-        note.value = poop.path; 
+        note.value = poop.path;
         note.addEventListener('click', triggerSound(poop.path));
     }
 };
