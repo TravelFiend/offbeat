7581341b83b638c313bc872ba90d3e4d73a4860d
TravelFiend <mikegrace274@gmail.com>
2019-10-23 13:46:41 -0700

    deleted 2 unused lines

--- Utils/generateMetroSoundList.js
+++ Utils/generateMetroSoundList.js
@@ -10,11 +10,9 @@ export const generateMetroSoundList = (allSounds) => {
     const menu = document.createElement('select');
     menu.name = 'metronome-sound';
     menu.id = 'metronome-sound-menu';
-    // menu.textContent = 'metronome-sound';
 
     allSounds.forEach(obj => {
         menu.append(generateMetroSoundListItem(obj));
-
     });
     return menu;
 };
@@ -23,11 +21,9 @@ export const generateDownBeat = (allSounds) => {
     const menu = document.createElement('select');
     menu.name = 'downbeat-sound';
     menu.id = 'downbeat-sound-menu';
-    menu.textContent = 'downbeat-sound';
 
     allSounds.forEach(obj => {
         menu.append(generateMetroSoundListItem(obj));
-        
     });
     return menu;
 };
