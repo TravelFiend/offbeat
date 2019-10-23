ae3b071fb03d3ff9a16ddcd157837174e8a23591
Jacob Harrington <jharrington4@gmail.com>
2019-10-23 13:59:49 -0700

    updated saveSettings function

--- Common/storeUser.js
+++ Common/storeUser.js
@@ -8,8 +8,10 @@ export const storeUser = (user) => {
 export const saveSettings = (user) => {
     const metroSound = document.querySelector('#metronome-sound-menu');
     const keySound = document.querySelector('#keyboard-sound-menu');
+    const downBeat = document.querySelector('#downbeat-sound-menu');
     user.metroSoundIndex = metroSound.selectedIndex;
     user.keySoundIndex = keySound.selectedIndex;
+    user.downBeatIndex = downBeat.selectedIndex;
     const stringedUser = JSON.stringify(user);
 
     localStorage.setItem('user', stringedUser);
