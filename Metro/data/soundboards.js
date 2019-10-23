d6d4cfc6e2de51e4c3367b1610e4dc6a9c9ec9fe
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-10-23 09:32:37 -0700

    init branch sounds etc

--- Metro/data/soundboards.js
+++ Metro/data/soundboards.js
@@ -2,62 +2,62 @@
 const soundBoards = [
     [{
         name : 'c',
-        path: '../assets/middle-C-clarinet.wav', //path to assets and respective sound (piano to start?)
+        path: '../assets/pussycat/Pussycat.116.wav', //path to assets and respective sound (piano to start?)
         noteNumber: 0,
     },
     {
         name: 'c-sharp',
-        path: '../assets/middle-C-clarinet.wav',
+        path: '../assets/pussycat/Pussycat.498.wav',
         noteNumber: 1,
     },
     {
         name: 'd',
-        path: '../assets/middle-C-clarinet.wav',
+        path: '../assets/pussycat/Pussycat.531.wav',
         noteNumber: 2,
     },
     {
         name: 'd-sharp',
-        path: '../assets/middle-C-clarinet.wav',
+        path: '../assets/pussycat/Pussycat.3110.wav',
         noteNumber: 3,
     },
     {
         name: 'e',
-        path: '../assets/middle-C-clarinet.wav',
+        path: '../assets/pussycat/Pussycat.3311.wav',
         noteNumber: 4,
     },
     {
         name: 'f',
-        path: '../assets/middle-C-clarinet.wav',
+        path: '../assets/pussycat/Pussycat.116.wav',
         noteNumber: 5,
     },
     {
         name: 'f-sharp',
-        path: '../assets/middle-C-clarinet.wav',
+        path: '../assets/pussycat/Pussycat.154.wav',
         noteNumber: 6,
     },
     {
         name: 'g',
-        path: '../assets/middle-C-clarinet.wav',
+        path: '../assets/pussycat/Pussycat.195.wav',
         noteNumber: 7,
     },
     {
         name: 'g-sharp',
-        path: '../assets/middle-C-clarinet.wav',
+        path: '../assets/pussycat/Pussycat.299.wav',
         noteNumber: 8,
     },
     {
         name: 'a',
-        path: '../assets/middle-C-clarinet.wav',
+        path: '../assets/pussycat/Pussycat.372.wav',
         noteNumber: 9,
     },
     {
         name: 'a-sharp',
-        path: '../assets/middle-C-clarinet.wav',
+        path: '../assets/pussycat/Pussycat.393.wav',
         noteNumber: 10,
     },
     {
         name: 'b',
-        path: '../assets/middle-C-clarinet.wav',
+        path: '../assets/pussycat/Pussycat.457.wav',
         noteNumber: 11,
     }]
 ];
