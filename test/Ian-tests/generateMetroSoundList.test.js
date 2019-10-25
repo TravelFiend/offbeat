438d15549daaf592e5ea0650e5854f458f554731
Ian Andrewson <ianandrewson@gmail.com>
2019-10-24 19:23:05 -0700

    all tests pass, linted PASS TRAVIS, DAMN YOU

--- test/Ian-tests/generateMetroSoundList.test.js
+++ test/Ian-tests/generateMetroSoundList.test.js
@@ -1,18 +1,32 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
-import { generateMetroSoundList } from '../../Utils/generateMetroSoundList.js';
+import { generateMetroSoundList, generateMetroSoundListItem } from '../../Utils/generateMetroSoundList.js';
 import { metroSounds } from '../../assets/metro-sounds/metroSounds.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('should return entire list', function(assert) {
     //Arrange
     // Set up your parameters and expectations
     const sounds = metroSounds.slice(0, 2);
-    const expected = '<select name="metronome-sound" id="metronome-sound-menu">metronome-sound<option value="../assets/metro-sounds/rimshotshort.wav">rimshot</option><option value="../assets/metro-sounds/880hz 50ms.mp3">880hz</option></select>';
+    const expected = '<select name="metronome-sound" id="metronome-sound-menu"><option value="../assets/metro-sounds/rimshotshort.wav">rimshot</option><option value="../assets/metro-sounds/880hz 50ms.mp3">880hz</option></select>';
     //Act 
     // Call the function you're testing and set the result to a const
     const result = generateMetroSoundList(sounds);
+
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result.outerHTML, expected);
+});
+
+test('should return a list item', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const expected = '<option value="../assets/metro-sounds/rimshotshort.wav">rimshot</option>';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = generateMetroSoundListItem(metroSounds[0]);
 
     //Assert
     // Make assertions about what is expected valid result
--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,2 @@
 import './Ian-tests/bpmToMs.test.js';
-import './Ian-tests/generateMetroSoundListItem.test.js';
 import './Ian-tests/generateMetroSoundList.test.js';
